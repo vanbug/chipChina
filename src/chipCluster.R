ee67b4cf4abe85913b6f2af80690292d2a45a7af
Sukhdeep Singh <vanbug@gmail.com>
2011-09-15 15:52:15 +0200

    adding chipCluster looper for ultra big chip files

--- src/chipCluster.R
+++ src/chipCluster.R
@@ -6,7 +6,7 @@
 
 # importing libraries
 library('ShortRead');
-library('chipseq');
+library('chipseq'); # required for estimate.mean.fraglen
 
 # declaring variables
 chrs<-c();chipChr<-list();controlChr<-list();
@@ -16,27 +16,34 @@ control<-readline('Please enter the control location! ')
 control<-gsub('\'','',control)
 chip<-readline('Please enter the chip location! ')
 chip<-gsub('\'','',chip)
+chip2<-readline('Please enter the chip II location! ')
+chip2<-gsub('\'','',chip2)
+
 
 
 # reading control and chip data
 control<-readAligned(control,type="BAM");
 chip<-readAligned(chip,type="BAM");
+chip2<-readAligned(chip2,type="BAM");
 
 # printing info
 print (control);
 print (chip);
+print (chip2);
 
 # uniquely aligned data, dropping NA's
 controlP=control[which(is.na(control@strand)==F)]
 chipP=chip[which(is.na(chip@strand)==F)]
+chip2P=chip2[which(is.na(chip2@strand)==F)]
 
 # inbuilt filtering for alignQuality occurrenceFilter
 filt1<-chromosomeFilter("chr[0-9XYM]")
-filt2<-alignQualityFilter(25)
+filt2<-alignQualityFilter(25) # random quality filter selected on quality scores by bwa
 filt3<-occurrenceFilter(withSread=FALSE)
 filt<-compose(filt1,filt2,filt3)
 controlF<-controlP[filt(controlP)]
 chipF<-chipP[filt(chipP)]
+chip2F<-chip2P[filt(chip2P)]
 
 # sort chromosome levels 
 sortChr=function(x){
@@ -50,54 +57,63 @@ sortedchrlevel=paste(sortedchrlevel,"$",sep='')
 return (sortedchrlevel)
 }
 
+# sort chromosome levels
+controlLevel=sortChr(controlF@chromosome);
+chipLevel=sortChr(chipF@chromosome);
+chip2Level=sortChr(chip2F@chromosome)
+
 # simplified chromosome extraction - single chromosome, multiple sample extraction
 extractChr=function(control1level=NULL,control2level=NULL,chip1level=NULL,chip2level=NULL,chip3level=NULL,control1=NULL,control2=NULL,chip1=NULL,chip2=NULL,chip3=NULL){
 if (is.null(control1)==FALSE) {controlChr1=control1[(chromosomeFilter(control1level))(control1)]} else {controlChr1=NULL}
 if (is.null(control2)==FALSE) {controlChr2=control2[(chromosomeFilter(control2level))(control2)]} else {controlChr2=NULL}
 if (is.null(chip1)==FALSE)    {chipChr1=chip1[(chromosomeFilter(chip1level))(chip1)]} else {chipChr1=NULL}
-if (is.null(chip2)==FALSE) {chipChr2=chip2[(chromosomeFilter(chip2level))(chip2)]} else {chipChr2=NULL}
-if (is.null(chip3)==FALSE) {chipChr3=chip3[(chromosomeFilter(chip3level))(chip3)]} else {chipChr3=NULL}
-return (list(control1=controlChr1,control2=controlChr2,chip1=chipChr1,chip2=chipChr2,chip3=chipChr3))
+if (is.null(chip2)==FALSE) {chip2Chr=chip2[(chromosomeFilter(chip2level))(chip2)]} else {chip2Chr=NULL}
+if (is.null(chip3)==FALSE) {chip3Chr=chip3[(chromosomeFilter(chip3level))(chip3)]} else {chip3Chr=NULL}
+return (list(control1=controlChr1,control2=controlChr2,chip1=chipChr1,chip2=chip2Chr,chip3=chip3Chr))
 }
 
 # lappy usage - multiple chromosome, multiple sample extraction
-extractNucleosome=function(control1level=NULL,chip1level=NULL,control1=NULL,chip1=NULL){
-chrs<-mapply(extractChr,control1level=control1level,chip1level=chip1level,MoreArgs=list(control1=control1,chip1=chip1))
+extractNucleosome=function(control1level=NULL,chip1level=NULL,chip2level=NULL,control1=NULL,chip1=NULL,chip2=NULL){
+chrs<-mapply(extractChr,control1level=control1level,chip1level=chip1level,chip2level=chip2level,MoreArgs=list(control1=control1,chip1=chip1,chip2=chip2))
 return (chrs)
 }
 
-# sort chromosome levels
-#controlLevels=sortChr(control@chromosome)
-#chipLevels=sortChr(chip@chromosome)
-
 # extract chip's n control's chromosome specific data
-#extractedChrs<-extractNucleosome(controlLevels,chipLevels,controlP,chipP)
+extractedChrs<-extractNucleosome(controlLevel,chipLevel,chip2Level,control1=controlF,chip1=chipF,chip2=chip2F)
 
+# for only chip and control
+#extractedChrs<-extractNucleosome(controlLevel,chipLevel,control1=controlF,chip1=chipF)
 
 #fetching chromosome data using seq
-#controlChrs=extractedChrs[seq(1,length(extractedChrs),by=5)]
-#chipChrs=extractedChrs[seq(3,length(extractedChrs),by=5)]
+controlChrs=extractedChrs[seq(1,length(extractedChrs),by=5)]
+chipChrs=extractedChrs[seq(3,length(extractedChrs),by=5)]
+chip2Chrs=extractedChrs[seq(4,length(extractedChrs),by=5)]
 
 # unlisting chip and control chromosome data to remove head list - NO NEED WITH CURRENT CODE
-#controlChr=unlist(controlChr)
-#chipChr=unlist(chipChr)
+controlChrs=unlist(controlChrs)
+chipChrs=unlist(chipChrs)
+chip2Chrs=unlist(chip2Chrs)
 
 # variable declarations
-chip.norm.aln<-list(); control.norm.aln<-list()
-chip.fragment.size<-c(); control.fragment.size<-c()
-chip.cov<-c(); control.cov<-c()
+chip.norm.aln<-list(); control.norm.aln<-list(); chip2.norm.aln<-list()
+chip.fragment.size<-c(); chip2.fragment.size<-c(); control.fragment.size<-c()
+chip.cov<-c(); chip2.cov<-list(); control.cov<-c()
 
 # NORMALIZING LIBRARY SIZE FROM CHIP AND CONTROL - in GFP & MEN, control has more reads
 for (i in 1:length(chipChrs)){
 sizeControl<-length(controlChrs[[i]]@id)
 sizeChip<-length(chipChrs[[i]]@id)
+#sizeChip2<-length(chip2Chrs[[i]]@id)
 smallest.size<-min(sizeControl,sizeChip)
+#smallest.size2<-min(sizeControl,sizeChip2)
 chip.norm.aln[[i]]<-sample(chipChrs[[i]],smallest.size,replace=FALSE)
+#chip2.norm.aln[[i]]<-sample(chip2Chrs[[i]],smallest.size2,replace=FALSE)
 control.norm.aln[[i]]<-sample(controlChrs[[i]],smallest.size,replace=FALSE)
 print (paste("Done with Chr",i,sep=''))
 #print (chip.norm.aln[[i]])
 #print (control.norm.aln[[i]])
 chip.fragment.size=c(chip.fragment.size,round(estimate.mean.fraglen(chip.norm.aln[[i]],method="SISSR")))
+#chip2.fragment.size=c(chip2.fragment.size,round(estimate.mean.fraglen(chip2.norm.aln[[i]],method="SISSR")))
 control.fragment.size=c(control.fragment.size,round(estimate.mean.fraglen(control.norm.aln[[i]],method="SISSR")))
 }
 
@@ -108,11 +124,13 @@ seqlengths(Mmusculus)
 
 # calculating extend length
 chip.extend.length=chip.fragment.size-width(chip.norm.aln[[1]][1])
+chip2.extend.length=chip2.fragment.size-width(chip2.norm.aln[[1]][1])
 control.extend.length=control.fragment.size-width(control.norm.aln[[1]][1])
 
 # calculating coverage
 for (i in 1:length(chipChrs)){
 chip.cov<-c(chip.cov,coverage(chip.norm.aln[[i]],width=seqlengths(Mmusculus),extend=as.integer(chip.extend.length[i])))
+#chip2.cov<-c(chip2.cov,coverage(chip.norm.aln[[i]],width=seqlengths(Mmusculus),extend=as.integer(chip2.extend.length[i])))
 control.cov<-c(control.cov,coverage(control.norm.aln[[i]],width=seqlengths(Mmusculus),extend=as.integer(control.extend.length[i])))
 print (paste((length(chipChrs)-i),"chromosomes left",sep=''))
 }
@@ -131,8 +149,11 @@ plotControl.Coverage<-function(x,xlab="Position",ylab="Coverage",main="Control C
 plotChr=function(x,type){
 	for (i in 1:length(x)) {
 	if (type=="control") {jpeg(paste("chr",i,".jpg",sep=''))
-				plotControl.Coverage(x[[i]][[1]],sub=paste(type,"chr",i))} else {
-				plotChIP.Coverage(x[[i]][[1]])}
+				plotControl.Coverage(x[[i]][[1]],sub=paste(type,"chr",i))} 
+	if (type=="chip")    {jpeg(paste("chr",i,".jpg",sep=''))
+				plotChIP.Coverage(x[[i]][[1]],sub=paste(type,"chr",i))}
+	if (type=="chip2")   {jpeg(paste("chr",i,".jpg",sep='')) 
+				plotChIP.Coverage(x[[i]][[1]],sub=paste(type,"chr",i))}
 	dev.off()
 	}
 }
@@ -142,15 +163,17 @@ tracks=function(x){return (as(x,"RangedData"))}
 nozero=function(x){return (subset(x,score>0))}
 # obtaining tracks
 chip.cov.Track=lapply(chip.cov,tracks)
+chip2.cov.Track=lapply(chip2.cov,tracks)
 control.cov.Track=lapply(control.cov,tracks)
 chip.cov.no.zero=lapply(chip.cov.Track,nozero)
+chip2.cov.no.zero=lapply(chip2.cov.Track,nozero)
 control.cov.no.zero=lapply(control.cov.Track,nozero)
 
 # for cov.no.zero , score is present at chip.cov.zero@values@unlistData@listData$score
 
 chrs=c(paste('chr',seq(1,19),sep=''),'chrX','chrY','chrM')
 
-# writing bedGraphs using write.table for all chromosomes, x= chip.cov
+# writing bedGraphs using write.table for all chromosomes, x= chip.cov, type = chip
 bedGraph=function(cov,chrs,type){
 for (i in 1:length(cov)){
 	pf<-runValue(cov[[i]][[1]])>0
@@ -161,15 +184,16 @@ for (i in 1:length(cov)){
 }
 }
  
+# my way of writing bed files
 # exporting the coverage no zero files as bedGraph files for each chromosome
-exportBed=function(cov.no.zero,bedFileName){
-export(cov.no.zero,bedFileName,"bedGraph")
-}
+#exportBed=function(cov.no.zero,bedFileName){
+#export(cov.no.zero,bedFileName,"bedGraph")
+#}
 # have to run a loop instead of mapply so as to get distinct names for different chrmosome bedFiles
-for (i in 1:length(control.cov.no.zero)){
-exportBed(control.cov.no.zero[[i]],paste(controlLevels[i],".control",sep=''))
-exportBed(chip.cov.no.zero[[i]],paste(chipLevels[i],".chip",sep=''))
-}
+#for (i in 1:length(control.cov.no.zero)){
+#exportBed(control.cov.no.zero[[i]],paste(controlLevels[i],".control",sep=''))
+#exportBed(chip.cov.no.zero[[i]],paste(chipLevels[i],".chip",sep=''))
+#}
 
 # single line production of bed files is, distinct filename feature is sorted.
 #mapply(exportBed,chip.cov.no.zero,MoreArgs=list(bedFileName="chip"))
@@ -223,9 +247,12 @@ mapply(enrich,chip.peaks,MoreArgs=list(y=control.peaks))
 ##########################################
 #Dummy code for reading in large bam files
 chip=list()
+# import bs genome package
+library(BSgenome.Mmusculus.UCSC.mm9)
 chrs=c(paste("chr",seq(1,19),sep=''),paste("chr",c('X','Y','M'),sep=''))
 what=scanBamWhat()
-number=seq(1,27,by=7)
+number=seq(1,27,by=3)
+number=number[-length(number)]
 for (i in 1:(length(number)-1)){
 which<-GRanges((chrs[number[i]:number[i+1]]),ranges=IRanges((number[i]:number[i+1]),c(seqlengths(Mmusculus)[number[i]:number[i+1]])))
 if (i==2|i==3) {which=which[-1]}
@@ -233,6 +260,31 @@ params<-ScanBamParam(which=which,what=what)
 print (which)
 chip[[i]]=readAligned('/projects/globalscratch/sukhi/rotterdam/uniqueRot/s_1_1.fastq.chipseq.bowtie.SR.mm9.sam.srt.bam.unique',type="BAM",param=params)
 }
+
+##########################################
+# looping data per chromosome
+chip=list();control<-list()
+# import bs genome package
+library(BSgenome.Mmusculus.UCSC.mm9)
+library(chipseq)
+chrs=c(paste("chr",seq(1,19),sep=''),paste("chr",c('X','Y','M'),sep=''))
+what=scanBamWhat()
+number=seq(1,22,by=1)
+for(i in 1:length(number)){
+	which<-GRanges((chrs[number[i]]),ranges=IRanges(number[i],c(seqlengths(Mmusculus)[number[i]])))
+params<-ScanBamParam(which=which,what=what)
+chip[[i]]=readAligned('/projects/globalscratch/sukhi/rotterdam/pipe/uniqueRot/s_4_1.fastq.chipseq.bowtie.SR.mm9.sam.srt.bam.unique',type="BAM",param=params)
+control[[i]]=readAligned('/projects/globalscratch/sukhi/rotterdam/pipe/uniqueRot/s_5_1.fastq.chipseq.bowtie.SR.mm9.sam.srt.bam.unique',type="BAM",param=params)
+print(paste("Chromosome",i,"imported"))
+}
+
+# length of each chromosome
+chrLen=function(x){return(length(x@id))}
+chipchrLength=unlist(lapply(chip,chrLen))
+controlchrLength=unlist(lapply(control,chrLen))
+
+
+
 
 ##########################################
 
